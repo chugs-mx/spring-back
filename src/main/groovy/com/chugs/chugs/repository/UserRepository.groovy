9f1f1296e49d5f88ac74f61b3f8a97b113f7af5b
erickdjm <erick.dejesus1@alumno.buap.mx>
2025-02-11 18:45:20 -0600

    add: project structure entity, service, controller, repository

--- src/main/groovy/com/chugs/chugs/repository/UserRepository.groovy
+++ src/main/groovy/com/chugs/chugs/repository/UserRepository.groovy
@@ -2,7 +2,6 @@ package com.chugs.chugs.repository
 
 import com.chugs.chugs.entity.User
 import org.springframework.data.jpa.repository.JpaRepository
-import org.springframework.data.jpa.repository.Query
 import org.springframework.stereotype.Repository
 
 @Repository
