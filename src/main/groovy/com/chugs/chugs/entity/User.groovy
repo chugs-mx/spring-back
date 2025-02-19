d218d2b1cf2e92362be40f24f92355062107e4ae
Erick De Jesus <94492584+3rickDJ@users.noreply.github.com>
2025-02-18 20:16:15 -0600

    3 add all entities (#4)
    * add: all entities
    
    * resolves #3
    
    * add attribute datePayment to the payment entity
    
    * update attributes
    
    * add bridge entity ProductInventory
    
    ---------
    
    Co-authored-by: rosaainz <rosagsainzz@gmail.com>

--- src/main/groovy/com/chugs/chugs/entity/User.groovy
+++ src/main/groovy/com/chugs/chugs/entity/User.groovy
@@ -1,6 +1,6 @@
 package com.chugs.chugs.entity
 
-
+import jakarta.persistence.CascadeType
 import jakarta.persistence.Column
 import jakarta.persistence.Entity
 import jakarta.persistence.EnumType
@@ -8,7 +8,10 @@ import jakarta.persistence.Enumerated
 import jakarta.persistence.GeneratedValue
 import jakarta.persistence.GenerationType
 import jakarta.persistence.Id
+import jakarta.persistence.OneToMany
 import jakarta.persistence.Table
+
+import java.time.LocalDateTime
 
 @Entity
 @Table(name = "app_user")
@@ -17,17 +20,26 @@ class User {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "user_id")
     Long userId
+
     @Column(nullable = false, length = 100)
     String name
+
     @Column(nullable = false, unique = true, length = 100)
     String email
+
     @Column(nullable = false, length = 255, name = "password_hash")
     String passwordHash
+
+    @Column(name = "birth_date", nullable = false)
+    LocalDateTime birthDate
+
     @Enumerated(EnumType.STRING)
     @Column(nullable = false, name = "user_type")
     UserType userType
-//    @OneToMany(mappedBy = "User", cascade = CascadeType.ALL, orphanRemoval = true)
-//    List<OrderTable> orders = new ArrayList<>()
+
+    @OneToMany(mappedBy = "user", cascade = CascadeType.ALL, orphanRemoval = true)
+    List<OrderTable> orders = []
+
     enum UserType {
         ADMIN, STAFF, WAITER, CUSTOMER
     }
