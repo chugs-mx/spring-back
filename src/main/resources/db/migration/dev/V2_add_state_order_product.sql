835c5ff1360ab649d94d181cb6a293e688f876bf
rosaainz <rosagsainzz@gmail.com>
2025-02-23 15:42:49 -0600

    add column order_date to the ticket table and column tip to the payment table for accounts that split their payment

--- src/main/resources/db/migration/dev/V2_add_state_order_product.sql
+++ src/main/resources/db/migration/dev/V2_add_state_order_product.sql
@@ -5,3 +5,11 @@ add column order_status VARCHAR(15) NOT NULL;
 -- ADD COLUMN notified TO THE order_product
 alter table order_product
 add column notified BOOLEAN DEFAULT FALSE;
+
+-- ADD COLUMN order_date TO THE ticket
+alter table ticket
+add column order_date TIMESTAMP DEFAULT CURRENT_TIMESTAMP;
+
+-- ADD COLUMN tip TO THE payment
+alter table payment
+add column tip DECIMAL(12,2) NOT NULL DEFAULT 0.00;
