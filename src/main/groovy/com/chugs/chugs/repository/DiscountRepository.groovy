cef92d27196507df135e1849c03345a9cc599fa1
rosaainz <rosagsainzz@gmail.com>
2025-02-25 23:29:11 -0600

    test failed for method getCurrentDiscount, filtering discounts with invalid date ranges

--- src/main/groovy/com/chugs/chugs/repository/DiscountRepository.groovy
+++ src/main/groovy/com/chugs/chugs/repository/DiscountRepository.groovy
@@ -4,12 +4,12 @@ import com.chugs.chugs.entity.Discount
 import org.springframework.data.jpa.repository.JpaRepository
 import org.springframework.stereotype.Repository
 
-import java.time.LocalDate
+import java.time.LocalDateTime
 
 @Repository
 interface DiscountRepository extends JpaRepository<Discount,Long>{
     // Obtain existing discounts
-    List<Discount> findByStartDateBeforeAndEndDateAfter(LocalDate todayStart,LocalDate todayEnd)
+    List<Discount> findByStartDateBeforeAndEndDateAfter(LocalDateTime startDate, LocalDateTime endDay)
 
     // Find discount by name
     Optional<Discount> findByName(String name)
