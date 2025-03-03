30ed140ff6589f4a915a90b4207fb9d046237401
rosaainz <rosagsainzz@gmail.com>
2025-03-02 22:16:15 -0600

    Successful test for method processPayments: single payment and different payments

--- src/test/groovy/com/chugs/chugs/Service/PaymentServiceSpec.groovy
+++ src/test/groovy/com/chugs/chugs/Service/PaymentServiceSpec.groovy
@@ -4,14 +4,15 @@ import com.chugs.chugs.entity.Payment
 import com.chugs.chugs.entity.Ticket
 import com.chugs.chugs.repository.PaymentRepository
 import com.chugs.chugs.repository.TicketRepository
-import org.springframework.data.rest.webmvc.ResourceNotFoundException
 import spock.lang.Specification
+
 import java.time.LocalDateTime
 
 class PaymentServiceSpec extends Specification {
     TicketRepository ticketRepository = Mock()
     PaymentRepository paymentRepository = Mock()
     PaymentService paymentService = new PaymentService(ticketRepository: ticketRepository, paymentRepository: paymentRepository)
+
 
     def "processSinglePaymentSuccessfully"() {
         given: "Create ticket and payment"
@@ -21,7 +22,7 @@ class PaymentServiceSpec extends Specification {
         // Payment already includes tip (total paid = subtotal tip)
         Payment payment = new Payment(
                 paymentMethod: Payment.PaymentMethod.CASH,
-                amountPaid: BigDecimal.valueOf(110), // Total with tip
+                amountPaid: BigDecimal.valueOf(100),
                 tip: BigDecimal.valueOf(10),
                 paymentDate: LocalDateTime.now()
         )
@@ -34,12 +35,36 @@ class PaymentServiceSpec extends Specification {
 
         then: "One payment and ticket are saved"
         // The ticket is saved and then when it changes to PAID it is saved again
-        2 * ticketRepository.save({ it.tip == BigDecimal.valueOf(10) && it.total == BigDecimal.valueOf(110) })
-        1 * paymentRepository.save({ it.ticket == ticket && it.amountPaid == BigDecimal.valueOf(110) })
+        2 * ticketRepository.save({ it.tip == BigDecimal.valueOf(10) && it.total == BigDecimal.valueOf(100) })
+        1 * paymentRepository.save({ it.ticket == ticket && it.amountPaid == BigDecimal.valueOf(100) })
 
         and: "The ticket status is updated"
         ticket.getTicketStatus() == Ticket.TicketStatus.PAID
     }
 
+    def"processDifferentPayments"(){
+        given: "Create a ticket and list of payments"
+        Long ticketId = 333L
+        Ticket ticket = new Ticket(ticketId: ticketId, subtotal: BigDecimal.valueOf(100), total: 100, tip: BigDecimal.ZERO, orderDate: LocalDateTime.now())
+        List<Payment> payments = [
+                new Payment(paymentMethod: Payment.PaymentMethod.CREDIT_CARD, amountPaid: BigDecimal.valueOf(30), tip: BigDecimal.valueOf(10)),
+                new Payment(paymentMethod: Payment.PaymentMethod.DEBIT_CARD, amountPaid: BigDecimal.valueOf(70), tip: BigDecimal.valueOf(10)),
+        ]
 
+        ticketRepository.findById(ticketId) >> Optional.of(ticket)
+        paymentRepository.findByTicket(ticket) >> payments
+
+        when:
+        paymentService.processPayments(ticket.ticketId, payments)
+
+        then:
+        2 * ticketRepository.save({ it.tip == BigDecimal.valueOf(20) && it.total == BigDecimal.valueOf(100) })
+
+        1 * paymentRepository.save({ it.ticket == ticket && it.amountPaid == BigDecimal.valueOf(30) })
+        1 * paymentRepository.save({ it.ticket == ticket && it.amountPaid == BigDecimal.valueOf(70) })
+
+        and:
+        ticket.getTicketStatus() == Ticket.TicketStatus.PAID
+
+    }
 }
