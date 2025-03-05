521985b7bbfe4c714d19c03dfa11492e992df1c1
rosaainz <rosagsainzz@gmail.com>
2025-03-05 14:11:47 -0600

    change column name category for inventory_category in sql

--- src/main/resources/db/migration/dev/V4_change_column_inventoryType_for_inventoryCategory_and_add_column_subcategory.sql
+++ src/main/resources/db/migration/dev/V4_change_column_inventoryType_for_inventoryCategory_and_add_column_subcategory.sql
@@ -4,4 +4,4 @@ add column subcategory VARCHAR(20) NOT NULL;
 
 -- RENAME COLUMN INVENTORY_TYPE TO CATEGORY
 alter table inventory
-rename column inventory_type to category;
+rename column inventory_type to inventory_category;
