96b913736588a5d653934e8f66df6b7dbe2a23d9
rosaainz <rosagsainzz@gmail.com>
2025-03-05 11:58:20 -0600

    Fixes #12 add column category and subcategory in entity and sql

---
+++ src/main/resources/db/migration/dev/V4_change_column_inventoryType_for_inventoryCategory_and_add_column_subcategory.sql
@@ -0,0 +1,7 @@
+-- ADD COLUMN SUBCATEGORY
+alter table inventory
+add column subcategory VARCHAR(20) NOT NULL;
+
+-- RENAME COLUMN INVENTORY_TYPE TO CATEGORY
+alter table inventory
+rename column inventory_type to category;
