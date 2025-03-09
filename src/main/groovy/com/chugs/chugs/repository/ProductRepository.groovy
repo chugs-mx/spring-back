86f38c21bf9cb8c32828cbda53a157ead31b87ff
rosaainz <rosagsainzz@gmail.com>
2025-03-09 11:23:09 -0600

    service and tests for ProductInventory

---
+++ src/main/groovy/com/chugs/chugs/repository/ProductRepository.groovy
@@ -0,0 +1,11 @@
+package com.chugs.chugs.repository
+
+import com.chugs.chugs.entity.Product
+import org.springframework.data.jpa.repository.JpaRepository
+import org.springframework.stereotype.Repository
+
+@Repository
+interface ProductRepository extends JpaRepository<Product, Long>{
+    
+
+}
