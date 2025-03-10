ef09143465834a858d0607cc8bce6d63d84a78a9
rosaainz <rosagsainzz@gmail.com>
2025-03-10 12:59:46 -0600

    changes method name findByProductAndInventory for findByProductIdAndInventory

--- src/main/groovy/com/chugs/chugs/repository/ProductInventoryRepository.groovy
+++ src/main/groovy/com/chugs/chugs/repository/ProductInventoryRepository.groovy
@@ -8,8 +8,8 @@ import org.springframework.stereotype.Repository
 
 @Repository
 interface ProductInventoryRepository extends JpaRepository<ProductInventory, Long>{
-    List<ProductInventory> findByProduct(Product product) // to obtain all the ingredients of a product
+    List<ProductInventory> findByProductId(Product product) // to obtain all the ingredients of a product
     List<ProductInventory> findByInventory(Inventory inventory) // to obtain all products using an input
-    Optional<ProductInventory> findByProductAndInventory(Product product, Inventory inventory) //relationship between a product and a ingredient
+    Optional<ProductInventory> findByProductIdAndInventory(Product product, Inventory inventory) //relationship between a product and a ingredient
 
 }
