c3b99ec92b331c6959b735f01103c787f95a3b27
erickdjm <erick.dejesus1@alumno.buap.mx>
2025-03-19 22:43:08 -0600

    refactor: rename UserController to UserController.java and improve method visibility

--- src/main/groovy/com/chugs/chugs/controller/UserController.java
+++ src/main/groovy/com/chugs/chugs/controller/UserController.java
@@ -1,34 +1,32 @@
-package com.chugs.chugs.controller
+package com.chugs.chugs.controller;
 
-import com.chugs.chugs.Service.UserService
-import com.chugs.chugs.entity.User
-import org.springframework.beans.factory.annotation.Autowired
-import org.springframework.data.domain.Page
-import org.springframework.web.bind.annotation.GetMapping
-import org.springframework.web.bind.annotation.PostMapping
-import org.springframework.web.bind.annotation.RequestBody
-import org.springframework.web.bind.annotation.RequestParam
-import org.springframework.web.bind.annotation.RestController
+import com.chugs.chugs.Service.UserService;
+import com.chugs.chugs.entity.User;
+import org.springframework.data.domain.Page;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
 @RestController
-class UserController {
-
-    @Autowired
-    UserService userService
+public class UserController {
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
 
     @GetMapping("/user")
-    Page<User> getPaginateUsers(@RequestParam(defaultValue = "0", name = "page") int page, @RequestParam(defaultValue ="10", name = "size") int size) {
-        userService.findAllUsers(page, size)
+    public Page<User> getPaginateUsers(@RequestParam(defaultValue = "0", name = "page") int page, @RequestParam(defaultValue = "10", name = "size") int size) {
+        return userService.findAllUsers(page, size);
     }
 
     @PostMapping("/user")
-    User createUser() {
+    public User createUser() {
         User user = new User();
-        user.name = "Erick"
-        user.email = "erickdjm@ellucia.com"
-        user.passwordHash = "ADHW@&da21273"
-        user.userType = User.UserType.ADMIN
-        userService.createUser(user)
+        user.setName("Erick");
+        user.setEmail("erickdjm@ellucia.com");
+        user.setPasswordHash("ADHW@&da21273");
+        user.setUserType(User.UserType.ADMIN);
+        return userService.createUser(user);
 
         //check if it is not already created
         //required fields
@@ -37,4 +35,10 @@ class UserController {
 
 
     }
+
+    public UserService getUserService() {
+        return userService;
+    }
+
+    private final UserService userService;
 }
