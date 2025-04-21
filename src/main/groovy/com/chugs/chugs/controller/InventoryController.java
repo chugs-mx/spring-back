f9bc24366674b51eab3014660cc8d5f64b577303
erickdjm <erick.dejesus1@alumno.buap.mx>
2025-04-21 17:45:06 -0600

    add: logger to Invenroty GET endpoint

--- src/main/groovy/com/chugs/chugs/controller/InventoryController.java
+++ src/main/groovy/com/chugs/chugs/controller/InventoryController.java
@@ -2,18 +2,21 @@ package com.chugs.chugs.controller;
 
 import com.chugs.chugs.Service.InventoryService;
 import com.chugs.chugs.entity.Inventory;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.data.domain.Page;
 import org.springframework.data.web.PagedModel;
 import org.springframework.http.ResponseEntity;
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
 
-@Controller
+@RestController
 @RequestMapping("/inventories")
 public class InventoryController {
     final private InventoryService inventoryService;
+    private static final Logger logger = LoggerFactory.getLogger(InventoryController.class);
 
     public InventoryController(InventoryService inventoryService) {
         this.inventoryService = inventoryService;
@@ -40,8 +43,10 @@ public class InventoryController {
                 subCategory
         );
         if (invetories.isEmpty()) {
+            logger.info("[Get] No inventories found");
             return ResponseEntity.noContent().build();
         }
+        logger.info("[Get] Inventories found: {}", invetories.getTotalElements());
         return ResponseEntity.ok(new PagedModel<>(invetories));
     }
 
