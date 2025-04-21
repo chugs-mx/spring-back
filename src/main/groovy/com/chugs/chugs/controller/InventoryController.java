02c39a87e759b5cd8f975f71b101b79f1824795f
erickdjm <erick.dejesus1@alumno.buap.mx>
2025-04-21 14:18:55 -0600

    add: Inventory Controller

---
+++ src/main/groovy/com/chugs/chugs/controller/InventoryController.java
@@ -0,0 +1,49 @@
+package com.chugs.chugs.controller;
+
+import com.chugs.chugs.Service.InventoryService;
+import com.chugs.chugs.entity.Inventory;
+import org.springframework.data.domain.Page;
+import org.springframework.data.web.PagedModel;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@Controller
+@RequestMapping("/inventories")
+public class InventoryController {
+    final private InventoryService inventoryService;
+
+    public InventoryController(InventoryService inventoryService) {
+        this.inventoryService = inventoryService;
+    }
+
+
+    @GetMapping("/")
+    ResponseEntity<PagedModel<Inventory>> getInventories(
+            @RequestParam(name = "page", defaultValue = "0") int page,
+            @RequestParam(name = "size", defaultValue = "10") int size,
+            @RequestParam(name = "sort", defaultValue = "") String sort,
+            @RequestParam(name = "asc", defaultValue = "true") boolean asc,
+            @RequestParam(name = "category", required = false) String category,
+            @RequestParam(name = "subcategory", required = false) String subCategory,
+            @RequestParam(name = "search", required = false) String search
+    ) {
+        Page<Inventory> invetories = inventoryService.getInventoriesWithPaginationSortingAndFiltering(
+                page,
+                size,
+                search,
+                sort,
+                asc,
+                category,
+                subCategory
+        );
+        if (invetories.isEmpty()) {
+            return ResponseEntity.noContent().build();
+        }
+        return ResponseEntity.ok(new PagedModel<>(invetories));
+    }
+
+}
